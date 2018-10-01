96a7a403b5d8b1c48e14f44ce8988bb30484e496
aleksanderlj <aleksanderlj@hotmail.com>
2018-10-01 15:56:03 +0200

    Test for roll() lavet

--- src/terning/DiceTest.java
+++ src/terning/DiceTest.java
@@ -8,6 +8,17 @@ class DiceTest {
 
     @org.junit.jupiter.api.Test
     void roll() {
+
+        for(int i = 0;(i<=60000); i++){
+            boolean expected = true;
+            boolean actual = false;
+
+            int roll = minTest.roll();
+            if (roll>0 || roll<7) //Hvis terningen er en værdi fra 1-6 bliver actual true ellers forbliver den false
+                actual = true;
+            //System.out.println(roll);
+            assertEquals(expected,actual);
+        }
     }
 
     @org.junit.jupiter.api.Test

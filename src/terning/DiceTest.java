09eb9e300e5a875b75c090ab3884beda5febe8c9
aleksanderlj <aleksanderlj@hotmail.com>
2018-10-01 15:31:23 +0200

    Tilføjet minTest objekt til DiceTest

--- src/terning/DiceTest.java
+++ src/terning/DiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
 
+    Dice minTest = new Dice(); //Objekt til at køre metoder fra Dice.java fra
+
     @org.junit.jupiter.api.Test
     void roll() {
     }
